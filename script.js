662cdf4bf5bdfec281e672c8dd74ff961bd895fd
Ivan Yashin <newparadigma1@gmail.com>
2024-07-26 12:41:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+  let content;
+
+  console.log('browser init');
   const browser = await puppeteer.launch({
     executablePath: '/usr/bin/chromium',
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
+
+  console.log('page init');
   const page = await browser.newPage();
-  await page.goto('http://app-mongo');
+  console.log('page init goto');
+  await page.goto('http://app-mongo', {timeout: 5000});
   
-  const content = await page.content();
+  console.log('page.content');
+  content = await page.content();
+
+  console.log('content.includes adminer');
   if (!content.includes('adminer')) {
     throw new Error('The text "adminer" was not found on the page');
   }
 
+  console.log('content.includes mongo');
   if (!content.includes('mongo')) {
     throw new Error('The text "mongo" was not found on the page');
   }
 
+  console.log('page.click');
   await page.click('input[type="submit"][value="Enter"]');
 
-  await new Promise(resolve => setTimeout(resolve, 2000)); // Ждем 2 секунды
+  console.log('timeout');
+  await new Promise(resolve => setTimeout(resolve, 1000));
 
-  const h2Text = await page.evaluate(() => {
-    const h2 = document.querySelector('h2');
-    return h2 ? h2.textContent : null;
-  });
+  console.log('page.content');
+  content = await page.content();
 
-  if (h2Text !== 'Database: local') {
-    throw new Error('The text "Database: local" was not found in the h2 tag');
+  console.log('content.includes MongoDB (alpha)');
+  if (!content.includes('MongoDB (alpha)')) {
+    throw new Error('The text "MongoDB (alpha)" was not found on the page');
+  }
+
+  console.log('content.includes db-mongo');
+  if (!content.includes('db-mongo')) {
+    throw new Error('The text "db-mongo" was not found on the page');
+  }
+
+  console.log('content.includes Database: local');
+  if (!content.includes('Database: local')) {
+    throw new Error('The text "Database: local" was not found on the page');
   }
 
   console.log('OK');
